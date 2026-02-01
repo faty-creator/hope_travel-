28fdef78374b9c3e8da4319ed23cb97816ab4543
fz <agourramfatimaezzahra0@gmail.com>
2026-02-02 00:09:05 +0100

    Update file10

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -78,7 +78,7 @@ const Home: React.FC = () => {
   return (
     <div className="overflow-hidden">
       {/* Parallax Hero Section */}
-      <section className="relative h-[85vh] flex items-center justify-center overflow-hidden parallax" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&q=80&w=2000')" }}>
+      <section className="relative h-[85vh] flex items-center justify-center overflow-hidden parallax" style={{ backgroundImage: "url('https://www.pexels.com/video/sun-setting-down-855946/')" }}>
         <div className="absolute inset-0 bg-black/40 dark:bg-black/60"></div>
 
         <div className="relative z-10 text-center text-white px-4 max-w-4xl animate-fade-in-up">
